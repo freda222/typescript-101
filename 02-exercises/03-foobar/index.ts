80bb77947725e7cd5ee851eda978f3779316a675
Fredrik Andersson <freda@axis.com>
2022-01-10 11:54:11 +0100

    exercise: 3

--- 02-exercises/03-foobar/index.ts
+++ 02-exercises/03-foobar/index.ts
@@ -33,7 +33,17 @@
  * - x is any other case
  */
 const fooBar = (x: number): "foo" | "bar" | "foobar" | number => {
-    throw new Error("Not implemented")
+    if (x % 3 === 0 && x % 5  === 0) {
+        return "foobar"
+    }
+    if (x % 3 === 0) {
+        return "foo"
+    }
+    if (x % 5 === 0) {
+        return "bar"
+    }
+
+    return x
 }
 
 for (let number = 1; number <= 20; number++) {
@@ -46,4 +56,5 @@ for (let number = 1; number <= 20; number++) {
     console.log(result)
 }
 
-export {}
+export { }
+
